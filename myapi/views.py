41ba3d4ad90cf14813deb9ca7e70a8cd84bb66e3
Kerriea-star <ojared511@gmail.com>
2023-03-23 16:43:39 +0300

    views

--- myapi/views.py
+++ myapi/views.py
@@ -19,3 +19,19 @@ class ApprovalsView(viewsets.ModelViewSet):
     queryset = Approvals.objects.all()
     serializer_class = ApprovalsSerializers
 
+@api_view
+def apprevereject(request):
+    try:
+        mdl = joblib.load("C:/Users/ojare/AIML/Bank-Loan-Authentication/loan_model.pkl")
+        mydata = request.data
+        unit = np.array(list(mydata.values()))
+        unit = unit.reshape(1,-1)
+        scalers = joblib.load()
+        X = scalers.transform(unit)
+        y_pred = mdl.predict(X)
+        y_pred = (y_pred>0.58)
+        newdf = pd.DataFrame(y_pred, columns=["Status"])
+        newdf = newdf.replace({True: "Approved", False: "Rejected"})
+        return JsonResponse("Your Status is {}".format(newdf), safe=False)
+    except ValueError as e:
+        return Response(e.args[0], status.HTTP_400_BAD_REQUEST)
