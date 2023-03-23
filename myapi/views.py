7c0f5f8a172fca048f3c9eeb74bad4a3493f37aa
Kerriea-star <ojared511@gmail.com>
2023-03-23 16:30:18 +0300

    started creating views

--- myapi/views.py
+++ myapi/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from django.core import serializers
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonResponse
+from .models import Approvals
+from .serializers import ApprovalsSerializers
+import pickle
+import joblib
+import json
+import numpy as np
+from sklearn import preprocessing
+import pandas as pd
+
+class ApprovalsView(viewsets.ModelViewSet):
+    queryset = Approvals.objects.all()
+    serializer_class = ApprovalsSerializers
+
